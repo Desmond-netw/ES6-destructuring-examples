cb353f7da9104a70672f0a4b51ad34d8ae2eec86
Desmond-netw <dsquare200@gmail.com>
2025-02-02 22:11:32 +0200

    exmp01: destructure array of numbers

---
+++ dist/src/01-example-destructure.js
@@ -0,0 +1,7 @@
+"use strict";
+// basic exmple of js destructure app
+const numbers = [10, 20, 40];
+const [first, second, third] = numbers;
+console.log(first);
+console.log(second);
+console.log(third);
---
+++ src/01-example-destructure.ts
@@ -0,0 +1,9 @@
+// basic exmple of js destructure app
+
+const numbers: number[] = [10, 20, 40];
+
+const [first, second, third] = numbers;
+
+console.log(first);
+console.log(second);
+console.log(third);
